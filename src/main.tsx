465f10d379f41902b83827f969fad6c96fdca01e
JCoder <josesusanto147@gmail.com>
2024-08-09 21:57:14 +0700

    route + pages + components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.tsx'
 
+// seluruh routing ditaruh pada router
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Home/>
+}])
+
+// gunakan RouterProvider lalu masukkan value dari router
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      "primary-gold": '#FFD700',
+      "secondary-dark-gold": '#B8860B',
+      "accent-black": "#222224"
+    },
   },
   plugins: [],
 };
