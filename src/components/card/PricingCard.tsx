48f72d424c24af78acb3c15d7819b2ca12ab6823
Johevin Blesstowi <BS22-1@slc.net>
2024-08-13 12:29:05 +0700

    pricing card

--- src/components/card/PricingCard.tsx
+++ src/components/card/PricingCard.tsx
@@ -2,7 +2,7 @@ import MetalicButton from "../MetalicButton";
 
 export default function PricingCard() {
   return (
-    <div>
+    <div className="p-4 bg-secondary-black w-fit rounded-2xl">
       <MetalicButton text="Join Now" onClick={() => {}} />
     </div>
   );
