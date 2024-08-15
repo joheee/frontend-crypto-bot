3b5e6562d485a04088be15b37bacd4f16ff3d34d
Itsjoses <93019618+Itsjoses@users.noreply.github.com>
2024-08-16 00:55:26 +0700

    solve cannot deploy

--- src/components/Bullrun.tsx
+++ src/components/Bullrun.tsx
@@ -1,6 +1,5 @@
 import HeaderText from "./HeaderText";
 
-
 export default function Bullrun() {
   return (
     <div className="my-8">
@@ -13,7 +12,6 @@ export default function Bullrun() {
         </div>
         <div className="w-full flex tablet:flex-row flex-col items-center tablet:justify-start desktop:my-6 tablet:my-1 gap-8 tablet:gap-0">
           <div className="tablet:w-1/2 w-full desktop:h-56 tablet:h-40 h-28 flex justify-center">
-
             <img src="/early_bird.png" alt="" className="h-full w-auto" />
           </div>
           <div className="tablet:w-1/2 w-full text-white desktop:text-xl tablet:text-base tablet:text-start text-center text-sm tablet:pr-12 tablet:px-0 px-8">
@@ -30,9 +28,8 @@ export default function Bullrun() {
           <button className="custom-btn btn-3 desktop:h-16 desktop:w-72 tablet:h-14 tablet:w-56 h-12 w-48 rounded-full text-xl">
             Join Now
           </button>
-
         </div>
       </div>
-    </PaddingLayout>
+    </div>
   );
 }
